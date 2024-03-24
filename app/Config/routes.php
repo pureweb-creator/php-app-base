bf966c7bfd50933eb74e9442cdd963dcd21b7434
Aquamarine <roman.semenikhin99@gmail.com>
2024-03-24 15:14:57 +0200

    Updated docker environment

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -4,8 +4,3 @@ use App\Core\Router;
 use App\Controllers\HomeController;
 
 Router::get('/home', [HomeController::class, 'index']);
-Router::get('/directory-iterator', [HomeController::class, 'directoryIterator']);
-Router::post('/directory-iterator', [HomeController::class, 'directoryIteratorForm']);
-Router::post('/test2', function (){
-    echo 'hi';
-});
